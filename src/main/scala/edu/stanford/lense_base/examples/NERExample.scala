c8244796df5a8ac13e6995bb6f71171cf5887826
keenon <keenon@stanford.edu>
2015-04-27 18:27:32 -0700

    Wrote basic test using some CoNLL data for NER.

--- src/main/scala/edu/stanford/lense_base/examples/NERExample.scala
+++ src/main/scala/edu/stanford/lense_base/examples/NERExample.scala
@@ -4,6 +4,7 @@ import edu.stanford.lense_base.graph.GraphStream
 import edu.stanford.lense_base.task.{Task, TaskStream}
 
 import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
 /**
  * Created by keenon on 4/27/15.
@@ -12,52 +13,68 @@ import scala.collection.mutable.ListBuffer
  */
 class NERExample(classes : Set[String]) {
 
-  val taskStream = new TaskStream()
-
-  val nerLabelNodeType = taskStream.graphStream.makeNodeType(classes)
-  val nerNeighborFactorType = taskStream.graphStream.makeFactorType(List(nerLabelNodeType, nerLabelNodeType))
-
   def predictNER(tokenPOSPairs : List[(String, String)], simulateAskingHuman : (Int) => (String)) : List[String] = {
-
-    // Create a Task for the NER example
-
-    val task = new Task[List[String]](taskStream) {
-      val nodes : ListBuffer[GraphStream#Graph#Node] = ListBuffer()
-
-      override def populateGraph(graph: GraphStream#Graph): Unit = {
-        var lastNode : graph.Node = null
-        for (i <- 0 to tokenPOSPairs.size-1) {
-          val newNode = graph.Node(nodeType = nerLabelNodeType, features = Map("feat1" -> 1.0), payload = i)
-          if (lastNode != null) {
-            graph.Factor(nodes = List(lastNode, newNode), factorType = nerNeighborFactorType)
-          }
-          nodes += newNode
-          lastNode = newNode
-        }
-      }
-
-      override def taskCompleteCallback(assignments: Map[GraphStream#Graph#Node, String]): List[String] = {
-        // Do something with the answers
-        nodes.map(assignments(_)).toList
-      }
-
-      override def lossFunction(msDelay: Double,
-                                dollars: Double,
-                                errorProbability: Map[GraphStream#Graph#Node, Double]): Double = {
-        dollars + errorProbability.values.sum
-      }
-
-      override def queryHuman(node: GraphStream#Graph#Node): String = {
-        val index = node.payload.asInstanceOf[Int]
-        simulateAskingHuman(index)
-      }
-    }
-
-    taskStream.perform(task)
+    // For now, super hack to test that human simulation is working
+    (0 to tokenPOSPairs.size-1).map(simulateAskingHuman).toList
   }
 
 }
 
-object NERExample {
+object NERExample extends App {
+  def loadNER : List[List[(String,String,String)]] = {
+    val loadedData : ListBuffer[List[(String,String,String)]] = ListBuffer()
+    val currentSentence : ListBuffer[(String,String,String)] = ListBuffer()
 
+    for (line <- Source.fromFile("data/conll.iob.4class.train").getLines()) {
+      val parts = line.split("\t")
+      if (parts.size == 4) {
+        val word: String = parts(0)
+        val pos: String = parts(1)
+        val ner: String = parts(3)
+        currentSentence.+=((word, pos, ner))
+
+        if (word == ".") {
+          loadedData += currentSentence.toList
+          currentSentence.clear()
+        }
+      }
+    }
+
+    loadedData.toList
+  }
+
+  def testSystem(data : List[List[(String,String,String)]], epsilon : Double) : Double = {
+    val classes = data.flatMap(_.map(_._3)).distinct.toList
+    val random = new util.Random(42)
+
+    val ner = new NERExample(classes.toSet)
+
+    var correct : Double = 0
+    var incorrect : Double = 0
+
+    for (sentence <- data) {
+      val predictedTags = ner.predictNER(sentence.map(tuple => (tuple._1, tuple._2)), (i : Int) => {
+        // With probability epsilon we choose at random
+        if (random.nextDouble() < epsilon) {
+          classes(random.nextInt(classes.size))
+        }
+        // Otherwise we return the correct result
+        else {
+          sentence(i)._3
+        }
+      })
+
+      for (i <- 0 to sentence.size - 1) {
+        if (sentence(i)._3 == predictedTags(i)) correct += 1
+        else incorrect += 1
+      }
+    }
+
+    correct / (correct + incorrect)
+  }
+
+  val data = loadNER
+  println(data(1))
+
+  println(testSystem(data, 0.1))
 }
--- src/main/scala/edu/stanford/lense_base/task/Task.scala
+++ src/main/scala/edu/stanford/lense_base/task/Task.scala
@@ -8,11 +8,11 @@ import edu.stanford.lense_base.graph.GraphStream
  * This is how users specify a task, designed to be flexible for varying loss functions across the same GraphStream, etc
  */
 abstract class Task[ReturnType](taskStream : TaskStream) {
-  abstract def populateGraph(graph : GraphStream#Graph)
-  abstract def taskCompleteCallback(assignments : Map[GraphStream#Graph#Node, String]) : ReturnType
+  def populateGraph(graph : taskStream.graphStream.Graph)
+  def taskCompleteCallback(assignments : Map[taskStream.graphStream.Graph#Node, String]) : ReturnType
 
   // These are defined at the task level to default to the TaskStream values, but can be overridden if finer grained
   // control is required over the lossFunction and human querying process.
-  abstract def lossFunction(msDelay : Double, dollars : Double, errorProbability : Map[GraphStream#Graph#Node,Double]) : Double
-  abstract def queryHuman(node : GraphStream#Graph#Node) : String
+  def lossFunction(msDelay : Double, dollars : Double, errorProbability : Map[taskStream.graphStream.Graph#Node,Double]) : Double
+  def queryHuman(node : taskStream.graphStream.Graph#Node) : String
 }
